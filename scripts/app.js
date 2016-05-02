8b39934fadd4dc0d39485169066e58ae5dec6274
Josh English <josh@joshenglish.com>
2016-05-02 14:57:15 -0500

    Removed a bunch of files.  Added some Javascript

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
+window.onload = function () {
+  var currentdate = new Date();
+  var dateTime = "The time is currently: " + (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate() + "/"
+                + currentdate.getFullYear() + " @ "
+                + (currentdate.getHours()-12) + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+  console.log("Welcome to the my Portfolio.");
+  console.log("I hope you enjoy what you find here.");
+  console.log("Please note that you can reach to me at josh@joshenglish.com.");
+  console.log("Remember you can change the world with each line of code you write.")
+  console.log(dateTime);
+}
+
 $(function(){
     $(".element").typed({
         strings: ["Web Developer.^2000", "Entrepreneur.^2000","Growth Hacker.^2000", "Financial Guru.^2000"],
