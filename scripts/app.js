8e69645346426ed1785ee4a8f52933833bae96d4
Josh English <josh@joshenglish.com>
2016-05-02 14:38:38 -0500

    Made major changes to site

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".element").typed({
+        strings: ["Web Developer.^2000", "Entrepreneur.^2000","Growth Hacker.^2000", "Financial Guru.^2000"],
+        typeSpeed: 25,
+        loop: true
+    });
+});
+
