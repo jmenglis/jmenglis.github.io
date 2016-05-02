6f5393f5d522694ca6a669fa76f401585b6fc98f
Josh English <josh@joshenglish.com>
2016-05-02 16:02:41 -0500

    Finalizing the Portfolio

--- scripts/app.js
+++ scripts/app.js
@@ -15,7 +15,7 @@ window.onload = function () {
 
 $(function(){
     $(".element").typed({
-        strings: ["Web Developer.^2000", "Entrepreneur.^2000","Growth Hacker.^2000", "Financial Guru.^2000"],
+        strings: ["Web Developer.^2000", "Entrepreneur.^2000","Growth Hacker.^2000", "FinTech Guru.^2000"],
         typeSpeed: 25,
         loop: true
     });
