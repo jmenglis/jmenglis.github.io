f0b29aa4b3c3e5ef9ac791de1dd832a49ff28bad
Josh English <josh@joshenglish.com>
2017-07-16 00:40:04 -0500

    The environment is now rendering using VueJS

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,56 @@
-window.onload = function () {
+window.onload = function() {
   var currentdate = new Date();
-  var dateTime = "The time is currently: " + (currentdate.getMonth()+1) + "/"
-                + currentdate.getDate() + "/"
-                + currentdate.getFullYear() + " @ "
-                + (currentdate.getHours()-12) + ":"
-                + currentdate.getMinutes() + ":"
-                + currentdate.getSeconds();
-  console.log("Welcome to the my Portfolio.");
-  console.log("I hope you enjoy what you find here.");
-  console.log("Please note that you can reach to me at josh@joshenglish.com.");
-  console.log("Remember you can change the world with each line of code you write.")
+  var dateTime =
+    'The date is currently: ' +
+    (currentdate.getMonth() + 1) +
+    '/' +
+    currentdate.getDate() +
+    '/' +
+    currentdate.getFullYear();
+  console.log('Welcome to the my Portfolio.');
+  console.log('I hope you enjoy what you find here.');
+  console.log('Please note that you can reach to me at josh@joshenglish.com.');
+  console.log(
+    'Remember you can change the world with each line of code you write.'
+  );
   console.log(dateTime);
-}
+};
 
-$(function(){
-    $(".element").typed({
-        strings: ["Web Developer.^2000", "Builder.^2000","Growth Hacker.^2000", "FinTech Guru.^2000", "Maker.^2000"],
-        typeSpeed: 25,
-        loop: true
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: 'Josh English',
+    description:
+      'I am a Full Stack Software Engineer with passion for beautiful end products.  I want to make it easier for people to communicate with web and mobile applications while enjoying seamless experiences and great performance.  I enjoy music, natural language processing, coffee and good food.',
+    skills: {
+      languages: ['HTML', 'Javascript', 'C#', 'Ruby', 'Python'],
+      webdev: ['CSS3', 'SASS', 'LESS', 'PostCSS', 'Webpack 2.0', 'Nodejs', 'Express', 'Hapi', 'ReactJS', 'Redux', 'ImmutableJS', 'AngularJS', 'jQuery', '.NET', 'VueJS', 'Sinatra', 'Rails', 'Gulp', 'Grunt', 'RivetsJS', 'Handlebars'],
+      database: ['MongoDb', 'PostgreSQL', 'OracleSQL', 'Microsoft SQL Server', 'T-SQL', 'PL/SQL', 'MySQL'],
+      testing: ['Mocha', 'Chai', 'RSpec', 'Jest'],
+      other: ['Git', 'VCS'],
+      deployment: ['Docker', 'AWS', 'Linux', 'Azure', 'Ansible', 'Heroku'],
+    }
+  },
+  mounted() {
+    let typed = new Typed(this.$refs.typedElement, {
+      strings: [
+        'Web Developer.^2000',
+        'Builder.^2000',
+        'Growth Hacker.^2000',
+        'FinTech Guru.^2000',
+        'Maker.^2000',
+      ],
+      typeSpeed: 25,
+      loop: true,
     });
+  },
+  methods: {
+    isLast(obj, index) {
+      if (obj.length - 1 === index) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  }
 });
