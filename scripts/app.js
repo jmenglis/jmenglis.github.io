11100bdacf1fe3701fb3c5aed459f77dcbd917d2
Josh English <josh@joshenglish.com>
2017-10-31 21:59:23 -0500

    Adding in a tasty console treat

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  console.log('I currently work at:');
+  console.log('***Fitch Solutions***');
+  console.log('============================');
+  console.log('I have worked at the following companies:');
+  console.log('*Desirelist*');
+  console.log('*Hightower*');
+  console.log('*The Portrait Group*');
+  console.log('*Envestnet | Tamarac*');
+  console.log('*Mediant Communications*');
+  console.log('============================');
+};
