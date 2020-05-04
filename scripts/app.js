fa08fe821ba4cb41393b54c1a2d83b06eea3d1d0
Josh English <josh@joshenglish.com>
2020-05-04 10:23:20 -0700

    Updating console.log

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 window.onload = () => {
   console.log('I currently work at:');
-  console.log('***Fitch Solutions***');
+  console.log('***Amazon Web Services***');
   console.log('============================');
   console.log('I have worked at the following companies:');
+  console.log('*Fitch Solutions*');
   console.log('*Desirelist*');
   console.log('*Hightower*');
   console.log('*The Portrait Group*');
